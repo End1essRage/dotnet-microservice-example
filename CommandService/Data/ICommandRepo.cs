2c6e825d21229e0a018f0d2e48e0e0709756da9e
End1essRage <end1essrage@mail.ru>
2024-02-03 20:30:26 +0300

    Обновил CommandService, сконфигурировал DbContext, добавил репозиторий для работы с командами, добавил эндпоинты

---
+++ CommandService/Data/ICommandRepo.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+using CommandService.Models;
+
+namespace CommandService.Data
+{
+    public interface ICommandRepo
+    {
+        bool SaveChanges();
+
+        //Platforms
+        IEnumerable<Platform> GetAllPlatforms();
+        void CreatePlatform(Platform platform);
+        bool PlatformExists(int platformId);
+
+        //Commands
+        IEnumerable<Command> GetCommandsForPlatform(int platformId);
+        Command GetCommand(int platformId, int commandId);
+        void CreateCommand(int platformId, Command command);
+    }
+}
